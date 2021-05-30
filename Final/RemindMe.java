816ad9126301b60c174acb7eced5da2f8b7fe74e
krisvuong <81175669+krisvuong@users.noreply.github.com>
2021-05-29 23:08:58 -0400

    Change to option 2
    Parameters: start date and end date in YYYY/MM/DD format
    
    Returns String: "X,Y"
    X = days until start of assignment
    Y = duration (in days) of assignment

--- Final/RemindMe.java
+++ Final/RemindMe.java
@@ -10,110 +10,42 @@ class RemindMe{
   
   public static void main(String[] args){
     
-    //temp variables
-    String taskName = "[assignment name]";
-    String dueDate = "2021/05/25";  //(YYYY/MM/DD)
-    String dueTime = "23:59";       //(HH:mm) - 24h clock
+    //temporary variables
+    String startDay = "2021/06/27";  //(YYYY/MM/DD)
+    String endDay = "2021/09/29";
     
-    System.out.println(countdown(taskName, dueDate, dueTime));
+    System.out.println(countdown(startDay, endDay));
   }
   
   
-   
-  
-  //add methods here
-  
-  
-  public static String countdown(String taskName, String dueDate, String dueTime){
+  public static String countdown(String startDay, String endDay){
     
     //Declare necessary variables
-    long totalSec;  //used for calculations
-    
-    long diffYear;  //remaining number of years/months/days... until due date
-    long diffMonth;
-    long diffDay;
-    long diffHour;
-    long diffMin;
-    long diffSec;
-    
+    long daysTilStart;  //days until assignment start date
+    long daysDuration;  //duration of assignment (in days)
     String returnMsg = "";
+    Date start = new Date();  //initialize start date
+    Date end = new Date();  //initialize end date
+    Date today = new Date();  //get present date
     
-    Date due = new Date();  //initialize dates
-    String niceDue;  //final formatted due date
+    //Create date and time format (template)
+    SimpleDateFormat simple = new SimpleDateFormat ("yyyy/MM/dd");
     
-    //Create date and time formats (templates)
-    SimpleDateFormat simple = new SimpleDateFormat ("yyyy/MM/dd/HH:mm");
-    SimpleDateFormat pretty = new SimpleDateFormat ("EEEE MMM d, y, h:mm a");  //ex. "Tuesday May 25, 2021, 9:39 PM"
-    
-    //Get current date
-    Date today = new Date();
-    
-    //Format due date
+    //Parse String input dates into Date types
     try{
-      due = simple.parse(dueDate + "/" + dueTime);
+      start = simple.parse(startDay);
+      end = simple.parse(endDay);
     }
     catch(Exception e){
     }
-    niceDue = pretty.format(due); 
-    //add negative time
     
-    //Total seconds until due date and time
-    totalSec = (due.getTime() - today.getTime())/1000;  //method gives milliseconds since July 1 1970 (divide by 1000 to get seconds)
-    
-    //Find number of years/months/days... until due date and time
-    diffYear = totalSec/31536000;
-    diffMonth = (totalSec - (diffYear*31536000))/2635200;
-    diffDay = (totalSec-(diffYear*31536000)-(diffMonth*2635200))/86400;
-    diffHour = (totalSec-(diffYear*31536000)-(diffMonth*2635200)-(diffDay*86400))/3600;
-    diffMin = (totalSec-(diffYear*31536000)-(diffMonth*2635200)-(diffDay*86400)-(diffHour*3600))/60;
-    diffSec = totalSec-(diffYear*31536000)-(diffMonth*2635200)-(diffDay*86400)-(diffHour*3600)-(diffMin*60);
-    
+    //Calculate total days until start, and duration of assignment
+    daysTilStart = (start.getTime() - today.getTime())/86400000;  //method gives milliseconds since July 1 1970 (divide by 86400000 to get days)
+    daysDuration = (end.getTime() - start.getTime())/86400000;
     
     //Create return String
-    if(diffYear>0){
-      returnMsg += diffYear + " year";
-      if(diffYear>1){
-        returnMsg += "s";
-      }
-      returnMsg += ", ";
-    }
-    if(diffMonth>0){
-      returnMsg += diffMonth + " month";
-      if(diffMonth>1){
-        returnMsg += "s";
-      }
-      returnMsg += ", ";
-    }
-    if(diffDay>0){
-      returnMsg += diffDay + " day";
-      if(diffDay>1){
-        returnMsg += "s";
-      }
-      returnMsg += ", ";
-    }
-    if(diffHour>0){
-      returnMsg += diffHour + " hour";
-      if(diffHour>1){
-        returnMsg += "s";
-      }
-      returnMsg += ", ";
-    }
-    if(diffMin>0){
-      returnMsg += diffMin + " minute";
-      if(diffMin>1){
-        returnMsg += "s";
-      }
-      returnMsg += ", ";
-    }
-    if(diffSec>0){
-      returnMsg += diffSec + " second";
-      if(diffSec>1){
-        returnMsg += "s";
-      }
-      returnMsg += ", ";
-    }
-    returnMsg += "until " + taskName + " is due on " + niceDue;
-    
+    returnMsg = daysTilStart + "," + daysDuration;
+
     //Return countdown message
     return returnMsg;
   }
